d8dab1dff17b23ad51aca3e3f316bf93b7c38ad2
dalilataldo <dalila.taldo@gmail.com>
2024-05-09 17:33:07 +0200

    add: price

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 const km = prompt('Quanti km vuoi percorrere?');
 const age = prompt('Indica la tua età');
 
-const price = km * 0.21
+let price = km * 0.21
+
+// va applicato uno sconto del 20% per i minorenni if age < 18
+// va applicato uno sconto del 40 % per gli over 65 if age > 65
+
+if (age < 18) {
+    price = (price * 0.8);
+} else if (age > 65) {
+    price = (price * 0.6);
+}
 
 console.log(price);
+document.getElementById('my_id').innerHTML = price;
